1ad2733c27cd8e5462b899e632ccfcddfb96d76e
Wallace Custódio Batista <wallacecbatista@gmail.com>
2021-10-26 11:38:37 -0300

    Delete laco.js

--- laco.js
+++
@@ -1,9 +0,0 @@
-function taboada() {
-    for (contador = 0; contador <= 10; contador++) {
-    resultado = 9 * contador ;
-    console.log('9 X ' + contador + '= ' + resultado + '\n');
-        
-    }
-    
-}
-console.log(taboada());
